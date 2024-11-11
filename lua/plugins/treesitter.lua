c074e8551cb0a775ce50163456ee19fafe19caab
Vadim Klimov <Vadim.Klimov@gmail.com>
2024-11-11 18:10:10 +0000

    Treesitter: add Nu parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "json",
-      "yaml",
-      "xml",
-      "markdown",
-    },
-    sync_install = false,
-    auto_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "json",
+        "yaml",
+        "xml",
+        "markdown",
+        "nu",
+      },
+      sync_install = false,
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
